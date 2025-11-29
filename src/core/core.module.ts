4101e8461e75ba1c92a56f71dc5a29de6f457f23
Guiliano Rangel <guiliano@gmail.com>
2025-11-29 08:43:37 -0300

    feat: update NestJS and AngularCDK

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -24,7 +24,7 @@ import { AuditSubscriber } from './database/subscribers/audit.subscriber';
         TypeOrmModule.forRoot({
             type: 'postgres',
             host: process.env.DB_HOST || 'localhost',
-            port: parseInt(process.env.DB_PORT || '5434'),
+            port: parseInt(process.env.DB_PORT || '5433'),
             username: process.env.DB_USERNAME || 'postgres',
             password: process.env.DB_PASSWORD || 'pg123',
             database: process.env.DB_DATABASE || 'lab_crud2',
